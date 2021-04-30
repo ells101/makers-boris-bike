04e880baf57156458bdcc230cd2e3be0b1a648d5
Elliott <ells101@hotmail.co.uk>
2021-04-30 15:36:09 +0100

    chapter 14 complete

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,14 +1,17 @@
 require_relative 'bike' # require doesn't work here, need require relative
 class DockingStation #  used when you referring to a file thats relative to 
-  attr_reader :bike  
+  attr_reader :bikes 
+  def initialize
+    @bikes = []
+  end
   def release_bike # the current file you are working on (basically, within the same project directory).
-    raise "ERROR! You can't do that... Docking Station is empty." if bike.nil?
+    raise "ERROR! You can't do that... Docking Station is empty." if bikes.empty?
     
     Bike.new
 
   end
   def dock(new_bike)
-    raise "ERROR! Docking station is full." if bike
-    @bike = new_bike
+    raise "ERROR! Docking station is full." if bikes.length >= 20
+    @bikes << new_bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,6 +4,7 @@ describe DockingStation do
   subject { DockingStation.new }
     it { is_expected.to respond_to(:release_bike) }
   end
+
   describe 'if a bike has been docked, then release a bike ' do
     #arrange
     station = DockingStation.new
@@ -14,18 +15,18 @@ describe DockingStation do
     subject { station.release_bike }
     it { is_expected.to respond_to(:working?) }
   end
-  
+
   describe '#dock' do
     #Arrange
     station = DockingStation.new
-    bike = Bike.new    
     #act
-    station.dock(bike)
+    20.times { station.dock(Bike.new) }
     #assert
     it 'raises an error when station is full' do
-      expect { station.dock(bike) }.to raise_error
+      expect { station.dock(Bike.new) }.to raise_error
     end
   end 
+
   describe '#release_bike' do
     #Arrange
     station = DockingStation.new
@@ -40,17 +41,16 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock)}
   end
 
-  describe '#bike' do
+  describe '#bikes' do
     it 'responds with a bike that has been docked' do
       # Arrange
       station = DockingStation.new
       bike = Bike.new
-      
       # Act
       station.dock(bike)
-
+      bikes = station.bikes
       # Assert
-      expect(station.bike).to eq(bike)
+      expect(station.bikes).to eq(bikes)
     end
   end
 end
